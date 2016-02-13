bba3bfe3f0f8ba59a022aa7db1281e0a43939870
Bill <bill.rousseau@outlook.com>
2016-02-13 12:48:14 +0100

    Bug de l'image corrigé en cas de défaite

--- movetoplay/www/js/factories/gameFct.js
+++ movetoplay/www/js/factories/gameFct.js
@@ -1,19 +1,18 @@
 movetoplay.factory('gameFct', function(dataFct, $interval) {
 	return {
 		'nbLoop': 4,
-		'index': 0,
 		'timer': {},
 		'startGame': function() {
 			if (dataFct.isClickable == 1)
 			{
+				var index = 0;
 				dataFct.imgUrl = "img/logo.png";
 				dataFct.scanIsActive = true;
 				dataFct.imgDisplay = 100 / this.nbLoop;
 				dataFct.title = "Trouvez l'image mystère autour de vous";
 				dataFct.instruction = "Déplacez-vous et scanner le QR-Code pour savoir si vous avez gagné";
 				dataFct.isClickable = 0;
-				this.index += 1;
-				var index = this.index;
+				index += 1;
 				var nbLoop = this.nbLoop;
 
 				this.timer = $interval(function() {
@@ -23,7 +22,6 @@ movetoplay.factory('gameFct', function(dataFct, $interval) {
 						dataFct.instruction = "Pour réessayer cliquez sur l’image.";
 						dataFct.scanIsActive = false;
 						dataFct.imgDisplay = 100;
-						dataFct.imgUrl = "img/No.png";
 						dataFct.isClickable = 1;
 					}
 					else
@@ -33,7 +31,7 @@ movetoplay.factory('gameFct', function(dataFct, $interval) {
 						dataFct.imgDisplay += (100 / nbLoop);
 						index += 1;
 					}
-				}, 5000, nbLoop - 1);
+				}, 3000, nbLoop - 1);
 				dataFct.isClickable = isClickable;
 			}
 		}
--- movetoplay/www/js/factories/qrcodeFct.js
+++ movetoplay/www/js/factories/qrcodeFct.js
@@ -1,4 +1,4 @@
-movetoplay.factory('qrcodeFct', function($interval, $ionicPlatform, gameFct, dataFct) {
+movetoplay.factory('qrcodeFct', function($interval, gameFct, dataFct) {
 	return {
 		'scanner': function() {
 			// alert("ok for scan");
@@ -7,36 +7,27 @@ movetoplay.factory('qrcodeFct', function($interval, $ionicPlatform, gameFct, dat
 				{
 					if (res.text == dataFct.qrValue)
 					{
-						// $ionicPlatform.ready(function() {
-							// $interval.cancel(gameFct.timer);
-							dataFct.title = "Bravo, vous avez gagné!";
-							dataFct.instruction = "Vous pouvez aller chercher votre cadeau ! Pour rejouer cliquez sur l’image";
-							dataFct.scanIsActive = false;
-							dataFct.imgDisplay = 100;
-							dataFct.imgUrl = "img/Yes.png";
-							dataFct.isClickable = 1;
-							if (gameFct.nbLoop < 8)
-								gameFct.nbLoop += 2;
-								// window.plugins.NativeAudio.preloadSimple('sound', 'audio/victoire.mp3');
-								// setTimeout(function() {
-								// window.plugins.NativeAudio.play('sound');
-								//     }, 100);
-						// });
+						$interval.cancel(gameFct.timer);
+						dataFct.title = "Bravo, vous avez gagné!";
+						dataFct.instruction = "Vous pouvez aller chercher votre cadeau ! Pour rejouer cliquez sur l’image";
+						dataFct.scanIsActive = false;
+						dataFct.imgDisplay = 100;
+						dataFct.isClickable = 1;
+						if (gameFct.nbLoop < 8)
+							gameFct.nbLoop += 2;
 					}
 					else
 					{
-						// $interval.cancel(gameFct.timer);
+						$interval.cancel(gameFct.timer);
 						dataFct.title = "Désolé, ce n'est pas la bonne image. Perdu!";
 						dataFct.instruction = "Pour réessayer cliquez sur l’image.";
 						dataFct.imgDisplay = 100;
 						dataFct.scanIsActive = false;
-						dataFct.imgUrl = "img/No.png";
 						dataFct.isClickable = 1;
 					}
 				}
 				else
 					alert("QR-Code non reconnu. Veuillez réessayer");
-				alert(res.cancelled);
 			}, function(res) {
 				alert("Erreur");
 			});
