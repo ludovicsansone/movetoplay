bddd1bc52de193e4da056ce0df472308a98fc02a
Bill <bill.rousseau@outlook.com>
2016-02-14 13:46:12 +0100

    Test du plugin iBeacon

--- movetoplay/www/js/controller.js
+++ movetoplay/www/js/controller.js
@@ -1,48 +1,42 @@
 movetoplay.controller('ctrl', ['$scope', '$interval', '$translate', 'qrcodeFct', 'gameFct', 'dataFct', 'audioFct', function($scope, $interval, $translate, qrcodeFct, gameFct, dataFct, audioFct) {
 	   
-	    //Instanciation des factories
-	    $scope.qrcodeFct = qrcodeFct;
-	    $scope.gameFct = gameFct;
-	    $scope.dataFct = dataFct;
-	    $scope.audioFct = audioFct;
+    //Instanciation des factories
+    $scope.qrcodeFct = qrcodeFct;
+    $scope.gameFct = gameFct;
+    $scope.dataFct = dataFct;
+    $scope.audioFct = audioFct;
 
 
-	   	// Déroulement du jeu
-	    $scope.startGame = function() {
-	    	$scope.gameFct.startGame();
-	    };
+   	// Déroulement du jeu
+    $scope.startGame = function() {
+    	$scope.gameFct.startGame();
+    };
 
-    	var proximityUUID= "19841986-1992-1995-2016-423323332442";
-    	var identifier = "Move To Play";
-    	var minor = 1;
-    	var major = 8128;
-    	var mtpRegion = new ibeacon.Region({
-    		uuid: proximityUUID
-    	});
-    	
-	    $scope.scanBeacons = function() {
-	    	ibeacon.startRangingBeaconsInRegion({
-	    		region: mtpRegion,
-	    		didRangeBeacons: function(result) {
-	    			if (result.beacons[0].rssi < -70)
-	    			{
-	    				alert('Too far!')
-	    			}
-	    			else
-	    			{
-	    				alert('Close!')
-	    			}
-	    			// alert(result.beacons[0].rssi);
-	    		}
-	    	})
-	    }
-	   	// Scan et vérification du QR-Code
-		$scope.scanner = function() {
-			$scope.qrcodeFct.scanner();
-		};
+	var createBeacon = function() {
 
-		$scope.audioFct.loadSound('claps', 'audio/claps.mp3');
-		$scope.audioFct.loadSound('defaite_temps', 'audio/defaite_temps.mp3');
-		$scope.audioFct.loadSound('defaite_qrcode', 'audio/defaite_qrcode.mp3');
-		$scope.audioFct.loadSound('change', 'audio/change.mp3');
+	    var uuid = "19841986-1992-1995-2016-423323332442"; // mandatory
+	    var identifier = 'Move To Play'; // mandatory
+	    var minor = 1; // optional, defaults to wildcard if left empty
+	    var major = 8128; // optional, defaults to wildcard if left empty
+
+	    // throws an error if the parameters are not valid
+	    var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major, minor);
+	    return beaconRegion;   
+	}
+
+	$scope.scanBeacons = function() {
+		var region = createBeacon();
+		for (item in region)
+			alert(item);
+	};
+
+   	// Scan et vérification du QR-Code
+	$scope.scanner = function() {
+		$scope.qrcodeFct.scanner();
+	};
+
+	$scope.audioFct.loadSound('claps', 'audio/claps.mp3');
+	$scope.audioFct.loadSound('defaite_temps', 'audio/defaite_temps.mp3');
+	$scope.audioFct.loadSound('defaite_qrcode', 'audio/defaite_qrcode.mp3');
+	$scope.audioFct.loadSound('change', 'audio/change.mp3');
 }]);
